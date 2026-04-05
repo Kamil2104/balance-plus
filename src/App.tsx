6048bb970228a6786635cf5d0cfe0b5e7557715c
Kamil Niewiadomski <kamilniewiadomski18@gmail.com>
2026-04-05 18:50:22 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-500 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Balance+</h1>
+    </div>
+  );
+}
+
+export default App;
