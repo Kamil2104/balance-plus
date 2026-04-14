e403b4464995a3a34a770bdeadd6c98549f887cc
Kamil Niewiadomski <kamilniewiadomski18@gmail.com>
2026-04-14 17:07:40 +0200

    Adjust layout of App component for improved responsiveness and visual consistency

--- src/App.tsx
+++ src/App.tsx
@@ -35,11 +35,11 @@ function App() {
   return (
     <div className="min-h-screen bg-black/95 text-white">
       <div className="w-full mx-auto px-4 sm:px-6 lg:px-8 xl:px-10 2xl:px-12 py-10 lg:py-14 flex flex-col gap-8 lg:gap-10">
-        <div className="flex flex-col sm:flex-row items-stretch gap-5 sm:gap-6 lg:gap-7 xl:gap-8">
-          <div className="flex-1 basis-0 sm:flex-[0.4] flex">
+        <div className="flex flex-col md:flex-col lg:flex-row items-stretch gap-5 sm:gap-6 lg:gap-7 xl:gap-8">
+          <div className="flex-1 basis-0 lg:flex-[0.4] flex">
             <DataInput accounts={accounts} onChange={updateAccount} usdPln={usdPln} />
           </div>
-          <div className="flex-1 basis-0 sm:flex-[0.6] flex">
+          <div className="flex-1 basis-0 lg:flex-[0.6] flex">
             <DataView accounts={accounts} usdPln={usdPln} />
           </div>
         </div>
